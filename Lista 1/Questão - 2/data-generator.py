ac68c36d23920108b8df8cf401d5b971f386bc05
SAndradeTC <satc1624@gmail.com>
2020-10-22 03:11:26 -0300

    graph

--- Lista 1/Questão - 2/data-generator.py
+++ Lista 1/Questão - 2/data-generator.py
@@ -3,17 +3,17 @@ import random
 import numpy as np
 random.seed(0) #incializa semente para garantir que sempre sejam obtidos os mesmo conjuntos de dados
 
-samples=400
+samples=1000
 file_input = open('./data_x.txt', 'w')
 file_output = open('./data_y.txt', 'w')
 
 for i in range(samples):
-    for k in range (0,5):
+    for k in range (0,4):
         x = random.uniform(0, 4)
         y = math.sin(math.pi * x) / (math.pi * x)
         file_input.write(str(x))
         file_output.write(str(y))
-        if(k!=4):
+        if(k!=3):
             file_input.write(', ')
             file_output.write(', ')
     if(i!=samples-1):
