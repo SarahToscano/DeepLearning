fa603f46f51d7ddecdc88454a5e0236ba69da006
SAndradeTC <satc1624@gmail.com>
2020-10-22 02:11:50 -0300

    0.45 accuracy

--- Lista 1/Questão - 2/data-generator.py
+++ Lista 1/Questão - 2/data-generator.py
@@ -8,10 +8,14 @@ file_input = open('./data_x.txt', 'w')
 file_output = open('./data_y.txt', 'w')
 
 for i in range(samples):
-    x = random.uniform(0, 4)
-    y = math.sin(math.pi * x) / (math.pi * x)
-    file_input.write(str(x))
-    file_output.write(str(y))
+    for k in range (0,5):
+        x = random.uniform(0, 4)
+        y = math.sin(math.pi * x) / (math.pi * x)
+        file_input.write(str(x))
+        file_output.write(str(y))
+        if(k!=4):
+            file_input.write(', ')
+            file_output.write(', ')
     if(i!=samples-1):
         file_input.write('\n')
         file_output.write('\n')
